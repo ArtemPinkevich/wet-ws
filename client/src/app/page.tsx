3919d5c11c59fac7fbbbbf25e1219f0bce3d3ded
AkaikinMD <AkaikinMD@elcomplus.ru>
2025-06-19 17:59:17 +0700

    базовая реализация сокет соединения

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,95 +1,93 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { ChangeEvent, useState } from "react";
+import {
+  closeConnectSocket,
+  openConnectSocket,
+  sendMessageText,
+} from "./socketHelper";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [messages, setMessage] = useState<string[]>([]);
+  const [text, setText] = useState<string>("");
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const handleChangeText = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value != "") {
+      setText(event.target.value);
+    }
+  };
+
+  const handleSendText = () => {
+    if (socket && text.length != 0) {
+      sendMessageText(socket, text);
+      setMessage([...messages, text]);
+      setText("");
+    }
+  };
+
+  const connect = () => {
+    if (!socket) {
+      setSocket(openConnectSocket());
+    }
+  };
+
+  const disconnect = () => {
+    if (socket) {
+      closeConnectSocket(socket);
+      setSocket(null);
+    }
+  };
+
+  if (socket) {
+    socket.onmessage = (mes) => {
+      const data = mes.data;
+      setMessage((mes) => [...mes, data]);
+    };
+  }
+
+  return (
+    <div style={{ display: "flex", gap: "5rem" }}>
+      <div>
+        <input
+          type="text"
+          value={text}
+          style={{
+            width: "100px",
+            height: "50px",
+          }}
+          onChange={(e) => handleChangeText(e)}
+        />
+        <button onClick={handleSendText}>Send text</button>
+
+        <button
+          onClick={connect}
+          style={{
+            width: "max-content",
+            height: "30px",
+            backgroundColor: "green",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Connect
+        </button>
+        <button
+          onClick={disconnect}
+          style={{
+            width: "max-content",
+            height: "30px",
+            backgroundColor: "red",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Disconnect
+        </button>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2>MESSAGE:</h2>
+        {messages.map((message, index) => (
+          <span key={index}>{message}</span>
+        ))}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import { WebSocketServer } from "ws";
 
 const port = process.env.PORT || 3002;
 
@@ -8,6 +9,32 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+const clients = new Set();
+
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  console.log("Новый клиент подключился!");
+
+  const sendMessageOtherClient = (message, sender) => {
+    clients.forEach((client) => {
+      if (client !== sender && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  };
+
+  ws.on("message", (message) => {
+    console.log("Получено сообщение: ", message.toString());
+    sendMessageOtherClient(message, ws);
+  });
+
+  ws.on("close", () => {
+    console.log("Клиент отключился!");
+    clients.delete(ws);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
